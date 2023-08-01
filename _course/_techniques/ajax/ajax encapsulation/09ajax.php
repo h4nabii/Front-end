46758ea0a648d0e8f1c09488d6ef62d2b448dd6e
h4nabii <leon_xenos.xu@foxmail.com>
2023-08-01 16:28:28 +0800

    tech ajax update

--- _course/_techniques/ajax/ajax encapsulation/09ajax.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-    $uname=$_GET['username'];
-    $mima=$_GET['password'];
-    echo '后台返回的数据'.$uname.$mima;
-?>
--- _course/_techniques/ajax/encapsulation/server/02.php
+++ _course/_techniques/ajax/encapsulation/server/02.php
@@ -1,7 +1,6 @@
 <?php
-
+    header("Access-Control-Allow-Origin: *");
     //模拟请求--等待时间
     sleep(3);//睡3秒之后--执行下面的内容
     echo '我是后台最新的数据';
-
 ?>
